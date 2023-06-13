5aebf6662973a70a382752b2e779225cc5cae9ac
sage <sage.lane@netwisedata.com>
2023-06-12 17:11:12 -0700

    update

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,22 @@
 // Adapted from https://blog.logrocket.com/how-to-set-up-node-typescript-express/
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
-const port = 5000
+const port = 5000;
 
 type Person = {
-  name: string
-  age: number 
-}
+  name: string;
+  age: number;
+};
 
-app.get('/', (req: Request<{}, {}, Person>, res: Response) => {
-  const person = req.body
+app.get("/", (req: Request<{}, {}, Person>, res: Response) => {
+  const person = req.body;
   res.send(person);
 });
 
-app.get('/job', () => {})
+// Receives batch input from sources.
+// Need to filter data
+app.post("/", () => {});
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
