713bb6501e4f09133b7a9f27cee45380c3750a3f
sage <sage.lane@netwisedata.com>
2023-06-15 14:11:27 -0700

    updates

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,12 @@
 // Adapted from https://blog.logrocket.com/how-to-set-up-node-typescript-express/
 import express, { Express, Request, Response } from "express";
+import bodyParser from "body-parser";
 import { getEmployees, getSalaries } from "./utils";
 
 // Goal: Create an api for HR to use to manage company employees.
 
 const app: Express = express();
+const jsonParser = bodyParser.json();
 const port = 5000;
 
 // Task -
@@ -15,7 +17,11 @@ const port = 5000;
 //   3) Merges this data into a single array and returns it in the form {id, fullName, salary }
 //   4) For portability, dynamically generate and console.log() an SQL string using the IDs submitted in the post request
 //      that will select all ids with a salary greater than 75k and cast the salary to the appropriate numeric value as a new column.
-app.get("/", async (req: Request<{}, {}, Array<number>>, res: Response) => {});
+app.get(
+  "/",
+  jsonParser,
+  async (req: Request<{}, {}, Array<number>>, res: Response) => {}
+);
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
